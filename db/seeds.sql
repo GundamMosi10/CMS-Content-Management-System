c7c80f31afe4c24fa5bb2c5345b4cdb7c60cfbef
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-22 17:08:45 -0500

    changed around the seeds a little bit to fit the structure that I was going for

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,7 +2,8 @@ INSERT INTO department (name)
 VALUES ("HR"), ("Operations"), ("Marketing")
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("HR Representative", 50000, 1), ("Engineer", 75000, 2), ("Senior Developer", 95000, 3)
+VALUES ("HR Representative", 50000, 1), ("Senior Developer", 95000, 2), ("Marketing Strategist", 70000, 3)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ben", "Clarkson", 1, NULL), ("John", "Smith", 2, NULL), ("Kayle", "Lawrence", 3, NULL)
+VALUES ("Ben", "Clarkson", 1, NULL), ("Kayle", "Lawrence", 2, NULL), ("John", "Smith", 3, NULL)
+
