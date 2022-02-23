4fde13ec410a12a86d7d682118cde1080aa36e55
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-23 14:57:07 -0500

    added a prompt in the index.js to start off the what the user would like to do, and added the .env to the gitignore

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 require('dotenv').config()
+const fs = require('fs'); //Filesystem
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
 
+//create the connection to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    database: 'content_db',
+    password: process.env.DB_PASS
+  },
+  console.log(`Connected to the content_db`)
+);
 
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'content_db',
-  password: process.env.DB_PASS
-});
+const menu = [
+  {
+    name: "menu",
+    type: "list",
+    message: "What would you like to do?",
+    choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Finished"]
+  }
+]
 
-connection.connect(err => {
+// view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
+
+
+
+
+
+
+db.connect(err => {
     if(err) throw err
     start()
 });
 
 function start(){
 };
+
