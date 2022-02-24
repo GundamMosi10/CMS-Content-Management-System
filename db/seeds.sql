a06a97a8fa1faa91c9b3531ad5697dce997272fc
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-23 19:23:43 -0500

    reworked the prompt menu for the main menu, then added a switch prompt to go through each different user choice that will be connected to different functions that will show certain prompts. Added the console.table dependency

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,8 +2,8 @@ INSERT INTO department (name)
 VALUES ("HR"), ("Operations"), ("Marketing")
 
 INSERT INTO role (title, salary, department_id)
-VALUES ("HR Representative", 50000, 1), ("Senior Developer", 95000, 2), ("Marketing Strategist", 70000, 3)
+VALUES ("HR Representative", 50000, 1), ("Senior Developer", 95000, 2), ("Marketing Strategist", 70000, 3), ("Junior Developer", 75000, 4)
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("Ben", "Clarkson", 1, NULL), ("Kayle", "Lawrence", 2, NULL), ("John", "Smith", 3, NULL)
+VALUES ("Ben", "Clarkson", 1, NULL), ("Kayle", "Lawrence", 2, 1), ("John", "Smith", 3, NULL), ("Devin", "Parker", 4, NULL)
 
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const fs = require('fs'); //Filesystem
 const inquirer = require('inquirer');
 const mysql = require('mysql2');
+const cTable = require('console.table');
 
 //create the connection to database
 const db = mysql.createConnection(
@@ -14,14 +15,76 @@ const db = mysql.createConnection(
   console.log(`Connected to the content_db`)
 );
 
-const menu = [
-  {
-    name: "menu",
-    type: "list",
-    message: "What would you like to do?",
-    choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Finished"]
-  }
-]
+const promptMenu = () => {
+  return inquirer.prompt([
+    {
+      type: "list",
+      name: "mainMenu",
+      message: "What would you like to do?",
+      choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Finished"]
+    }
+  ])
+  .then(userChoices => {
+    switch (userChoices.mainMenu) {
+      case "View All Departments":
+        promptDepartments();
+        break;
+      case "View All Roles":
+        promptRoles();
+        break;
+      case "View All Employees":
+        promptEmployees();
+        break;
+      case "Add a Department":
+        promptNewDepartment();
+        break;
+      case "Add a Role":
+        promptNewRole();
+        break;
+      case "Add an Employee":
+        promptNewEmployee();
+        break;
+      case "Update an Employee Role":
+        promptUpdateEmployeeRole();
+        break;
+      case "Finished":
+        promptFinish();
+    }
+  });
+};
+
+
+
+promptMenu(); 
+
+
+// const promptDepartments = () => {
+//   return inquirer.prompt
+// }
+
+
+
+// const questions = [
+//   {
+//     name: "menu",
+//     type: "list",
+//     message: "What would you like to do?",
+//     choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role", "Finished"]
+//   },
+//   {
+//     type: "list",
+//     name: "department",
+//     message: "Which department would you like to view?",
+//     choices: ["HR", "Operations", "Marketing"]
+//   },
+//   {
+//     type: "list",
+//     name: "roles",
+//     message: "which role would you like to view",
+//     choices: [""]
+//   }
+// ]
+
 
 // view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
 
@@ -30,11 +93,11 @@ const menu = [
 
 
 
-db.connect(err => {
-    if(err) throw err
-    start()
-});
+// db.connect(err => {
+//     if(err) throw err
+//     start()
+// });
 
-function start(){
-};
+// function start(){
+// };
 
